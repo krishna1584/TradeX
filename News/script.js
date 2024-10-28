3307c8fa0fc6ce02e25c4bd6f6cf3e1155b52252
krishna1584 <abhi19ay@gmail.com>
2024-10-28 22:16:46 +0530

    Created Stock News Section

---
+++ News/script.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const API_KEY = '0da90f9ba1da45a2925b20377076ba52';
+    const url = "https://newsapi.org/v2/everything?q=";
+
+    // Load news specific to Indian stock market on page load
+    window.addEventListener('load', () => fetchNews('Indian Stock Market OR BSE OR NSE OR Sensex OR Nifty50'));
+
+    function reload() {
+        window.location.reload();
+    }
+
+    async function fetchNews(query) {
+        const res = await fetch(`${url}${query}&language=en&apikey=${API_KEY}`);
+        const data = await res.json();
+        bindData(data.articles.slice(0, 15)); // Limit to 15 articles
+    }
+
+    const searchButton = document.getElementById('search-button');
+    const searchText = document.getElementById('search-text');
+    searchButton.addEventListener('click', () => {
+        const query = searchText.value;
+        if (!query) return;
+        fetchNews(query + ' AND (BSE OR NSE OR Sensex OR Nifty50)');
+        curSelectedNav?.classList.remove('active');
+        curSelectedNav = null;
+    });
+});
